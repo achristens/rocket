9f3b6418d4dc6698859cfe83d1d0471af12eebec
Abby Christens <achristens@gmail.com>
2017-10-27 12:45:00 -0400

    test lift off returns false

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,35 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def test_name
+    @rocket.name = "Cayenne"
+    result = @rocket.name
+    expected = "Cayenne"
+    assert_equal(expected, result)
+  end
+
+  def test_flying_true
+    @rocket.lift_off
+    result = @rocket.flying?
+    expected = true
+    assert_equal(expected, result)
+  end
+
+  def test_lift_off
+    result = @rocket.lift_off
+    expected = true
+    assert_equal(expected, result)
+  end
+
+  def test_lift_off_false
+    @rocket.lift_off
+    result = @rocket.lift_off
+    expected = false
+    assert_equal(expected, result)
+  end
 end
